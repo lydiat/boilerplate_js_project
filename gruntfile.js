218fb2e7a247b78a8d10d1605281a785fe9e6cdb
Lydia Wagner <lydia.wagner@codeandtheory.com>
2016-08-08 08:34:44 -0400

    less verbose gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,33 +1,21 @@
 // Gruntfile.js
 
-// our wrapper function (required by grunt and its plugins)
-// all configuration goes inside this function
+// all configuration goes inside this wrapper function (required by grunt and plugins)
 module.exports = function(grunt) {
 
-  // ===========================================================================
-  // CONFIGURE GRUNT ===========================================================
-  // ===========================================================================
   grunt.initConfig({
 
-    // get the configuration info from package.json ----------------------------
-    // this way we can use things like name and version (pkg.name)
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'), // get the configuration info from package.json
 
-    // all of our configuration will go here
-
-        // configure jshint to validate js files -----------------------------------
-    jshint: {
+    jshint: {// configure jshint to validate js files
       options: {
         reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
       },
 
-      // when this task is run, lint the Gruntfile and all js files in src
-      build: ['Gruntfile.js', 'js/*.js']
+      build: ['Gruntfile.js', 'js/*.js'] // when this task is run, lint the Gruntfile and all js files in src
     },
 
-
-    // compile less stylesheets to css -----------------------------------------
-    less: {
+    less: { // compile less stylesheets to css 
       build: {
         files: {
           'css/style.css': 'css/style.less'
@@ -35,15 +23,11 @@ module.exports = function(grunt) {
       }
     },
 
-    // configure watch to auto update ----------------
-    watch: {
+    watch: {    // configure watch to auto update 
       
-      // for stylesheets, watch css and less files 
-      // only run less and cssmin stylesheets: { 
       files: ['css/*.css', 'css/*.less'], 
       tasks: ['less'] ,
 
-      // for scripts, run jshint 
       scripts: { 
         files: 'js/*.js', tasks: ['jshint'] 
       } 
@@ -51,13 +35,9 @@ module.exports = function(grunt) {
 
   });
 
-  // ===========================================================================
-  // LOAD GRUNT PLUGINS ========================================================
-  // ===========================================================================
-
+// load grunt plugins
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
 
 };
